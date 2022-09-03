0329d10219d49edba4ff8362fa9cf615f91506a4
Feng Gu <fenggu2000@gmail.com>
2022-09-02 23:46:15 -0400

    get request for bots

--- diplomacy/server/run.py
+++ diplomacy/server/run.py
@@ -14,7 +14,7 @@
 #
 #  You should have received a copy of the GNU Affero General Public License along
 #  with this program.  If not, see <https://www.gnu.org/licenses/>.
-# ==============================================================================
+# =============================================================================
 """ Small module script to quickly start a server with pretty log-printing.
 
     You can stop the server with keyboard interruption (Ctrl+C). Usage:
@@ -29,10 +29,26 @@
 
 """
 import argparse
+import asyncio
+
 from diplomacy import Server
 from diplomacy.utils import constants
+import tornado.web
+import tornado.ioloop
+import json
 
-if __name__ == '__main__':
+
+class RequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        game = self.get_argument('game', None)
+        path = '../web/data/games/' + game + '.json'
+        with open(path) as f:
+            data = json.load(f)
+        self.write(data)
+        f.close()
+
+
+async def main():
     PARSER = argparse.ArgumentParser(description='Run server.')
     PARSER.add_argument('--port', '-p', type=int, default=constants.DEFAULT_PORT,
                         help='run on the given port (default: %s)' % constants.DEFAULT_PORT)
@@ -40,5 +56,20 @@ if __name__ == '__main__':
 
     try:
         Server().start(port=ARGS.port)
+        print('Server started on port %s.' % ARGS.port)
+
+        app = tornado.web.Application([
+            (r'/', RequestHandler),
+        ])
+
+        app.listen(8888)
+        print('Tornado started on port 8888.')
+        #tornado.ioloop.IOLoop.current().start()
+        await asyncio.Event().wait()
+
     except KeyboardInterrupt:
         print('Keyboard interruption.')
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
