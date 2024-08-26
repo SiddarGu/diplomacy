62e7c55072ada60c0dd84a0cf70388217626947d
Feng Gu <fenggu2000@gmail.com>
2024-08-26 15:52:34 -0400

    fix: this.games_phases[context.request.game_id][context.request.game_role] is null

--- diplomacy/web/src/diplomacy/client/connection.js
+++ diplomacy/web/src/diplomacy/client/connection.js
@@ -15,17 +15,17 @@
 //  with this program.  If not, see <https://www.gnu.org/licenses/>.
 // ==============================================================================
 /*eslint no-unused-vars: ["error", { "args": "none" }]*/
-import {STRINGS} from "../utils/strings";
-import {UTILS} from "../utils/utils";
-import {REQUESTS} from "../communication/requests";
-import {RESPONSES} from "../communication/responses";
-import {NOTIFICATIONS} from "../communication/notifications";
-import {RESPONSE_MANAGERS} from "./response_managers";
-import {NOTIFICATION_MANAGERS} from "./notification_managers";
-import {Future} from "../utils/future";
-import {FutureEvent} from "../utils/future_event";
-import {RequestFutureContext} from "./request_future_context";
-import {Diplog} from "../utils/diplog";
+import { STRINGS } from "../utils/strings";
+import { UTILS } from "../utils/utils";
+import { REQUESTS } from "../communication/requests";
+import { RESPONSES } from "../communication/responses";
+import { NOTIFICATIONS } from "../communication/notifications";
+import { RESPONSE_MANAGERS } from "./response_managers";
+import { NOTIFICATION_MANAGERS } from "./notification_managers";
+import { Future } from "../utils/future";
+import { FutureEvent } from "../utils/future_event";
+import { RequestFutureContext } from "./request_future_context";
+import { Diplog } from "../utils/diplog";
 
 class Reconnection {
     constructor(connection) {
@@ -37,31 +37,47 @@ class Reconnection {
 
     genSyncCallback(game) {
         const reconnection = this;
-        return ((serverSyncResponse) => {
-            reconnection.games_phases[game.local.game_id][game.local.game_role] = serverSyncResponse;
+        return (serverSyncResponse) => {
+            reconnection.games_phases[game.local.game_id][
+                game.local.game_role
+            ] = serverSyncResponse;
             ++reconnection.n_synchronized_games;
-            if (reconnection.n_synchronized_games === reconnection.n_expected_games)
+            if (
+                reconnection.n_synchronized_games ===
+                reconnection.n_expected_games
+            )
                 reconnection.syncDone();
-        });
+        };
     }
 
     reconnect() {
-        for (let waitingContext of Object.values(this.connection.requestsWaitingResponses))
+        for (let waitingContext of Object.values(
+            this.connection.requestsWaitingResponses
+        ))
             waitingContext.request.re_sent = true;
-        const lenWaiting = Object.keys(this.connection.requestsWaitingResponses).length;
+        const lenWaiting = Object.keys(
+            this.connection.requestsWaitingResponses
+        ).length;
         const lenBefore = Object.keys(this.connection.requestsToSend).length;
-        Object.assign(this.connection.requestsToSend, this.connection.requestsWaitingResponses);
+        Object.assign(
+            this.connection.requestsToSend,
+            this.connection.requestsWaitingResponses
+        );
         const lenAfter = Object.keys(this.connection.requestsToSend).length;
         if (lenAfter !== lenWaiting + lenBefore)
-            throw new Error('Programming error.');
+            throw new Error("Programming error.");
         this.connection.requestsWaitingResponses = {};
 
         const requestsToSendUpdated = {};
         for (let context of Object.values(this.connection.requestsToSend)) {
             if (context.request.name === STRINGS.SYNCHRONIZE)
-                context.future.setException(new Error('Sync request invalidated for game ID ' + context.request.game_id));
-            else
-                requestsToSendUpdated[context.request.request_id] = context;
+                context.future.setException(
+                    new Error(
+                        "Sync request invalidated for game ID " +
+                            context.request.game_id
+                    )
+                );
+            else requestsToSendUpdated[context.request.request_id] = context;
         }
         this.connection.requestsToSend = requestsToSendUpdated;
 
@@ -94,20 +110,49 @@ class Reconnection {
             let keep = true;
             if (REQUESTS.isPhaseDependent(context.request.name)) {
                 const request_phase = context.request.phase;
-                const server_phase = this.games_phases[context.request.game_id][context.request.game_role].phase;
-                if (request_phase !== server_phase) {
-                    context.future.setException(new Error(
-                        'Game ' + context.request.game_id + ': request ' + context.request.name +
-                        ': request phase ' + request_phase + ' does not match current server game phase '
-                        + server_phase + '.'));
-                    keep = false;
+                if (
+                    context.request.game_id !== null &&
+                    context.request.game_role !== null &&
+                    this.games_phases.hasOwnProperty(context.request.game_id) &&
+                    this.games_phases[context.request.game_id].hasOwnProperty(
+                        context.request.game_role
+                    ) &&
+                    this.games_phases[context.request.game_id][
+                        context.request.game_role
+                    ] !== null
+                ) {
+                    const server_phase =
+                        this.games_phases[context.request.game_id][
+                            context.request.game_role
+                        ].phase;
+                    if (request_phase !== server_phase) {
+                        context.future.setException(
+                            new Error(
+                                "Game " +
+                                    context.request.game_id +
+                                    ": request " +
+                                    context.request.name +
+                                    ": request phase " +
+                                    request_phase +
+                                    " does not match current server game phase " +
+                                    server_phase +
+                                    "."
+                            )
+                        );
+                        keep = false;
+                    }
                 }
             }
             if (keep)
                 requestsToSendUpdated[context.request.request_id] = context;
         }
-        Diplog.info('Keep ' + Object.keys(requestsToSendUpdated).length + '/' +
-            Object.keys(this.connection.requestsToSend).length + ' old request(s) to send.');
+        Diplog.info(
+            "Keep " +
+                Object.keys(requestsToSendUpdated).length +
+                "/" +
+                Object.keys(this.connection.requestsToSend).length +
+                " old request(s) to send."
+        );
         this.connection.requestsToSend = requestsToSendUpdated;
 
         for (let context of Object.values(requestsToSendUpdated)) {
@@ -116,7 +161,7 @@ class Reconnection {
 
         this.connection.isReconnecting.set();
 
-        Diplog.info('Done reconnection work.');
+        Diplog.info("Done reconnection work.");
     }
 }
 
@@ -148,7 +193,7 @@ class ConnectionProcessing {
         this.connection.socket.onclose = this.connection.onSocketClose;
         this.connection.currentConnectionProcessing = null;
         this.connection.isConnecting.set();
-        this.logger.info('Connection succeeds.');
+        this.logger.info("Connection succeeds.");
     }
 
     onSocketTimeout() {
@@ -156,11 +201,25 @@ class ConnectionProcessing {
             this.connection.socket.close();
             if (this.attemptIndex === UTILS.NB_CONNECTION_ATTEMPTS) {
                 this.connection.isConnecting.set(
-                    new Error(`${this.connection.isReconnecting.isWaiting() ? 'Reconnection' : 'Connection'} failed after ${UTILS.NB_CONNECTION_ATTEMPTS} attempts.`));
+                    new Error(
+                        `${
+                            this.connection.isReconnecting.isWaiting()
+                                ? "Reconnection"
+                                : "Connection"
+                        } failed after ${
+                            UTILS.NB_CONNECTION_ATTEMPTS
+                        } attempts.`
+                    )
+                );
                 return;
             }
-            this.logger.warn('Connection failing (attempt ' + this.attemptIndex + '/' +
-                UTILS.NB_CONNECTION_ATTEMPTS + '), retrying ...');
+            this.logger.warn(
+                "Connection failing (attempt " +
+                    this.attemptIndex +
+                    "/" +
+                    UTILS.NB_CONNECTION_ATTEMPTS +
+                    "), retrying ..."
+            );
             ++this.attemptIndex;
             setTimeout(this.tryConnect, 0);
         }
@@ -172,7 +231,10 @@ class ConnectionProcessing {
         try {
             this.connection.socket = new WebSocket(this.connection.getUrl());
             this.connection.socket.onopen = this.onSocketOpen;
-            this.timeoutID = setTimeout(this.onSocketTimeout, UTILS.ATTEMPT_DELAY_SECONDS * 1000);
+            this.timeoutID = setTimeout(
+                this.onSocketTimeout,
+                UTILS.ATTEMPT_DELAY_SECONDS * 1000
+            );
         } catch (error) {
             this.__on_error(error);
         }
@@ -180,16 +242,14 @@ class ConnectionProcessing {
 
     process() {
         this.connection.isConnecting.clear();
-        if (this.connection.socket)
-            this.connection.socket.close();
+        if (this.connection.socket) this.connection.socket.close();
         this.tryConnect();
         return this.connection.isConnecting.wait();
     }
 
     stop() {
         if (!this.isConnected) {
-            if (this.connection.socket)
-                this.connection.socket.onopen = null;
+            if (this.connection.socket) this.connection.socket.onopen = null;
             if (this.timeoutID) {
                 clearTimeout(this.timeoutID);
                 this.timeoutID = null;
@@ -201,9 +261,8 @@ class ConnectionProcessing {
 /** Class Connection (like Python class diplomacy.client.connection.Connection). **/
 export class Connection {
     constructor(hostname, port, useSSL) {
-        if (useSSL)
-            Diplog.info(`Using SSL.`);
-        this.protocol = useSSL ? 'wss' : 'ws';
+        if (useSSL) Diplog.info(`Using SSL.`);
+        this.protocol = useSSL ? "wss" : "ws";
         this.hostname = hostname;
         this.port = port;
         this.socket = null;
@@ -225,12 +284,12 @@ export class Connection {
         this.isReconnecting.set();
 
         /** Public events. **/
-        this.onReconnection = null;  // onReconnection()
+        this.onReconnection = null; // onReconnection()
         this.onReconnectionError = null; // onReconnectionError(error)
     }
 
     getUrl() {
-        return this.protocol + '://' + this.hostname + ':' + this.port;
+        return this.protocol + "://" + this.hostname + ":" + this.port;
     }
 
     onSocketMessage(messageEvent) {
@@ -246,48 +305,55 @@ export class Connection {
             const message = messageEvent.data;
             const jsonMessage = JSON.parse(message);
             if (!(jsonMessage instanceof Object)) {
-                Diplog.error('Unable to convert a message to a JSON object.');
+                Diplog.error("Unable to convert a message to a JSON object.");
                 return;
             }
             if (jsonMessage.request_id) {
                 const requestID = jsonMessage.request_id;
                 if (!this.requestsWaitingResponses.hasOwnProperty(requestID)) {
-                    Diplog.error('Unknown request ' + requestID + '.');
+                    Diplog.error("Unknown request " + requestID + ".");
                     return;
                 }
                 const context = this.requestsWaitingResponses[requestID];
                 delete this.requestsWaitingResponses[requestID];
                 try {
-                    context.future.setResult(RESPONSE_MANAGERS.handleResponse(context, RESPONSES.parse(jsonMessage)));
+                    context.future.setResult(
+                        RESPONSE_MANAGERS.handleResponse(
+                            context,
+                            RESPONSES.parse(jsonMessage)
+                        )
+                    );
                 } catch (error) {
                     context.future.setException(error);
                 }
-            } else if (jsonMessage.hasOwnProperty('notification_id') && jsonMessage.notification_id)
-                NOTIFICATION_MANAGERS.handleNotification(this, NOTIFICATIONS.parse(jsonMessage));
-            else
-                Diplog.error('Unknown socket message received.');
+            } else if (
+                jsonMessage.hasOwnProperty("notification_id") &&
+                jsonMessage.notification_id
+            )
+                NOTIFICATION_MANAGERS.handleNotification(
+                    this,
+                    NOTIFICATIONS.parse(jsonMessage)
+                );
+            else Diplog.error("Unknown socket message received.");
         } catch (error) {
             Diplog.error(error);
         }
     }
 
     onSocketClose(closeEvent) {
-        if (this.closed)
-            Diplog.info('Disconnected.');
+        if (this.closed) Diplog.info("Disconnected.");
         else {
-            Diplog.error('Disconnected, trying to reconnect.');
+            Diplog.error("Disconnected, trying to reconnect.");
             this.isReconnecting.clear();
             this.__connect()
                 .then(() => {
                     new Reconnection(this).reconnect();
-                    if (this.onReconnection)
-                        this.onReconnection();
+                    if (this.onReconnection) this.onReconnection();
                 })
-                .catch(error => {
+                .catch((error) => {
                     if (this.onReconnectionError)
                         this.onReconnectionError(error);
-                    else
-                        throw error;
+                    else throw error;
                 });
         }
     }
@@ -297,7 +363,10 @@ export class Connection {
             this.currentConnectionProcessing.stop();
             this.currentConnectionProcessing = null;
         }
-        this.currentConnectionProcessing = new ConnectionProcessing(this, logger);
+        this.currentConnectionProcessing = new ConnectionProcessing(
+            this,
+            logger
+        );
         return this.currentConnectionProcessing.process();
     }
 
@@ -319,18 +388,17 @@ export class Connection {
             if (!connection.requestsToSend.hasOwnProperty(requestID)) {
                 connection.requestsToSend[requestID] = requestContext;
             }
-            Diplog.info('Error occurred while sending a request ' + requestID);
+            Diplog.info("Error occurred while sending a request " + requestID);
             writeFuture.setException(error);
         };
         if (request.name === STRINGS.SYNCHRONIZE)
             this.isConnecting.wait().then(onConnected, onAnyError);
-        else
-            this.isReconnecting.wait().then(onConnected, onAnyError);
+        else this.isReconnecting.wait().then(onConnected, onAnyError);
         return writeFuture.promise();
     }
 
     connect(logger) {
-        Diplog.info('Trying to connect.');
+        Diplog.info("Trying to connect.");
         return this.__connect(logger);
     }
 
@@ -341,10 +409,12 @@ export class Connection {
     }
 
     authenticate(username, password) {
-        return this.send(REQUESTS.create('sign_in', {
-            username: username,
-            password: password
-        })).promise();
+        return this.send(
+            REQUESTS.create("sign_in", {
+                username: username,
+                password: password,
+            })
+        ).promise();
     }
 
     close() {
