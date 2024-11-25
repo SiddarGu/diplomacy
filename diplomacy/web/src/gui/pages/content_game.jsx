2c62a5e7c34b0976ab6eaaafd0c0027599233237
Alex Hedges <git@alexhedges.dev>
2024-11-25 16:15:29 -0500

    Simplify logic in `getSuggestedMessages()`

--- diplomacy/web/src/gui/pages/content_game.jsx
+++ diplomacy/web/src/gui/pages/content_game.jsx
@@ -1590,16 +1590,11 @@ export class ContentGame extends React.Component {
     }
 
     getSuggestedMessages(currentPowerName, protagnist, isAdmin, engine, globalMessages) {
-        let globalSuggestedMessages = [];
-
-        for (let m of globalMessages) {
-            if (m.type === "suggested_message") {
-                globalSuggestedMessages.push(m);
-            }
-        }
-
         const suggestedMessagesForCurrentPower =
-            globalSuggestedMessages.filter((msg) => {
+            globalMessages.filter((msg) => {
+                if (msg.type !== "suggested_message")
+                    return false;
+
                 if (!msg.message.includes(":") || !msg.message.includes("-"))
                     return false;
                 const ps = msg.message.split(":")[0].split("-");
@@ -1609,19 +1604,14 @@ export class ContentGame extends React.Component {
                 if (msg.message.split(":")[1] !== "message")
                     return false;
 
-                if (
-                    sender === currentPowerName &&
+                return sender === currentPowerName &&
                     recipient === protagnist &&
                     msg.phase === engine.phase &&
                     (isAdmin ||
                         !this.state.annotatedMessages.hasOwnProperty(
                             msg.time_sent
-                        ))
-                ) {
-                    return true;
-                }
-                return false;
-            }) || [];
+                        ));
+            });
 
         return suggestedMessagesForCurrentPower;
     }
